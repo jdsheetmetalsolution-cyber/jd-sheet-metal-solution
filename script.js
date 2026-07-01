2e59cd3030cf40b7d29f251e5a7f8d394214c72d
jdsheetmetalsolution-cyber <jdsheetmetalsolution@gmail.com>
2026-07-01 21:37:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,101 +1,21 @@
-// J&D Navigation Animation
+function updateClock(){
 
-const links = document.querySelectorAll(".nav-link");
+const india=new Date().toLocaleTimeString("en-IN",{
 
-let isFlying = false;
+timeZone:"Asia/Kolkata",
 
-links.forEach(link => {
+hour:"2-digit",
 
-    link.addEventListener("click", function(e){
+minute:"2-digit",
 
-        e.preventDefault();
-
-        if(isFlying) return;
-
-        isFlying = true;
-
-        const target = document.querySelector(this.getAttribute("href"));
-
-        if(!target){
-            isFlying = false;
-            return;
-        }
-
-        // Remove previous airplane
-        const oldPlane = document.getElementById("plane");
-        if(oldPlane) oldPlane.remove();
-
-        // Create airplane
-        const plane = document.createElement("img");
-
-        plane.id = "plane";
-
-        plane.src = "images/paper-plane.png";
-
-        document.body.appendChild(plane);
-
-        // Menu position
-        const menu = this.getBoundingClientRect();
-
-        const startX = menu.left + menu.width/2;
-
-        const startY = menu.top + window.scrollY + menu.height/2;
-
-        plane.style.left = startX + "px";
-        plane.style.top = startY + "px";
-
-        // Destination
-        const destination = target.getBoundingClientRect();
-
-        const endX = window.innerWidth/2;
-
-        const endY = destination.top + window.scrollY + 80;
-
-        // Fly airplane
-        plane.animate([
-
-            {
-                left:startX+"px",
-                top:startY+"px",
-                opacity:1
-            },
-
-            {
-                left:endX+"px",
-                top:endY+"px",
-                opacity:1
-            }
-
-        ],{
-
-            duration:1800,
-
-            easing:"ease-in-out",
-
-            fill:"forwards"
-
-        });
-
-        setTimeout(()=>{
-
-            window.scrollTo({
-
-                top:target.offsetTop-70,
-
-                behavior:"smooth"
-
-            });
-
-        },1800);
-
-        setTimeout(()=>{
-
-            plane.remove();
-
-            isFlying=false;
-
-        },2600);
-
-    });
+second:"2-digit"
 
 });
+
+document.getElementById("clock").innerHTML=india;
+
+}
+
+updateClock();
+
+setInterval(updateClock,1000);
