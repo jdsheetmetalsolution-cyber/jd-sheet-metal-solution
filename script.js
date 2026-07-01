aa5b575070b7acb842e4f4c5f5b761d1e7040384
jdsheetmetalsolution-cyber <jdsheetmetalsolution@gmail.com>
2026-07-01 23:16:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-function updateIndianTime() {
+<script>
+function updateTime(){
 
-    const options = {
-        timeZone: "Asia/Kolkata",
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-        hour12: true
-    };
+const now = new Date();
 
-    const time = new Date().toLocaleTimeString("en-IN", options);
+const time = now.toLocaleTimeString('en-IN',{
 
-    document.getElementById("live-time").textContent = time;
+hour:'2-digit',
+minute:'2-digit',
+second:'2-digit',
+hour12:true,
+timeZone:'Asia/Kolkata'
+
+});
+
+document.getElementById("live-time").textContent=time;
+
 }
 
-updateIndianTime();
-
-setInterval(updateIndianTime, 1000);
+updateTime();
+setInterval(updateTime,1000);
+</script>
